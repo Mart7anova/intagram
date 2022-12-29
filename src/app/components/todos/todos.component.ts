e53713962a1db0dc532f08c3fa33094f56ed1c96
Mart7anova <mart7anova@gmail.com>
2022-12-29 16:09:16 +0300

    Post и delete запросы

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -16,6 +16,13 @@ interface Todo {
 export class TodosComponent implements OnInit {
   todos: Todo[] = []
 
+  httpOptions = {
+    withCredentials: true,
+    headers: {
+      'api-key': 'd4e06bbb-2142-48c2-be24-68df57142f14',
+    },
+  }
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
@@ -24,14 +31,33 @@ export class TodosComponent implements OnInit {
 
   getTodos() {
     this.http
-      .get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', {
-        withCredentials: true,
-        headers: {
-          'api-key': 'd4e06bbb-2142-48c2-be24-68df57142f14',
-        },
-      })
+      .get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', this.httpOptions)
       .subscribe(res => {
         this.todos = res
+      })
+  }
+
+  createTodo() {
+    const randomNumber = Math.floor(Math.random() * 100)
+    const title = 'Angular' + randomNumber
+    this.http
+      .post<{ data: { item: Todo } }>(
+        'https://social-network.samuraijs.com/api/1.1/todo-lists',
+        { title },
+        this.httpOptions
+      )
+      .subscribe(res => {
+        const newTodo = res.data.item
+        console.log(res)
+        this.todos.unshift(newTodo)
+      })
+  }
+
+  deleteTodo(id: string) {
+    this.http
+      .delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`, this.httpOptions)
+      .subscribe(() => {
+        this.todos = this.todos.filter(t => t.id !== id)
       })
   }
 }
